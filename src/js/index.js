419d2e1dd6248fa1459fd38d0d3272cb174b9460
Anna Kubiak <ainakubiak@gmail.com>
2020-03-20 20:32:17 +0100

    complete third task

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,14 @@
 const clickMe = document.querySelector('#show-popup-form');
 const popup = document.querySelector('.popup');
 const popupEscape = document.querySelector('.popup-escape');
+const form = document.querySelector('.popup__form');
+const thank = document.querySelector('.thank-message');
+
+const afterSubmit = () => {
+  popup.style.display = 'none';
+  clickMe.style.display = 'none';
+  thank.style.display = 'block';
+};
 
 clickMe.addEventListener('click', () => {
   popup.style.display = 'block';
@@ -8,4 +16,11 @@ clickMe.addEventListener('click', () => {
 
 popupEscape.addEventListener('click', () => {
   popup.style.display = 'none';
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  form.reset();
+
+  setTimeout(afterSubmit, 3000);
 });
